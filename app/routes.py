fb00f0ec82e5e3ae215093055a38fa9f4553ce89
fducha <fducha@list.ru>
2018-02-22 03:42:55 +0300

    3 chapter

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -25,3 +26,16 @@ def index():
                            title='Home',
                            user=user,
                            posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Запрошен вход для пользователя {}, опция "Запомнить меня" = {}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect(url_for('index'))
+    return render_template('login.html',
+                           title='Вход',
+                           form=form)
